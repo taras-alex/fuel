f7aa08c0c5c1d75fa3c0f14dd39de1649725cb31
alex <alex@localhost>
2019-02-13 21:42:37 +0300

    правильное названиедля запаса хода #2

--- fuel.py
+++ fuel.py
@@ -4,6 +4,6 @@ def fuel(expense, stock):
     900.0
 
     """
-    resere = 0.1
-    result = (1 - resere) * (stock / expense) * 100
+    reserve = 0.1
+    result = (1 - reserve) * (stock / expense) * 100
     return result
